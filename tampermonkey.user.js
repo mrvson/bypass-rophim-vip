37db422838745ead3575f20cd8d31863b2507ff5
FireT <firetofficial@gmail.com>
2025-09-12 18:04:50 +0700

    upload

---
+++ tampermonkey.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Rophim Full VIP
+// @namespace    http://tampermonkey.net/
+// @version      1.1
+// @description  Bypass VIP + Coin trên Rophim
+// @author       FireT
+// @require      https://raw.githubusercontent.com/firetofficial/bypass-rophim-vip/refs/heads/main/rophim_vip.js
+// @match        *://www.rophim.me/*
+// @run-at       document-start
+// @grant        none
+// @icon         https://avatars.githubusercontent.com/u/120646974?s=48&v=4
+// ==/UserScript==
